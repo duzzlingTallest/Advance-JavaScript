6b046540c0c9b0654594fccc63bbdb0ecbf0bcb5
kaushal damani <kaushaldamani386@gmail.com>
2023-06-26 09:13:03 +0530

    string

---
+++ 10_String/main.js
@@ -0,0 +1,29 @@
+// -------------------- Strig --------------------
+
+let a = "Kaushal"  // create a string 
+
+console.log(a.length);
+console.log(a[0]);
+console.log(a[1]);
+
+let b = 'Jay'
+
+console.log(b.length);
+
+
+    //  -------------------- Template literals --------------------
+
+    let a1 = "nameste"
+    let a2 = "hello"
+    let a3 = "hola"
+
+    // nameste is the hind greeting and hello is the english greeting.
+
+    let sentance = `${a1} and ${a2} is same`
+
+    console.log(sentance);
+
+    // -------------------- Escapes Sequence Character --------------------
+
+    let fruite = 'banana\'s'
+    console.log(fruite);
